4a491e9570df81c8f472d8a3bc3952adec820848
John Hubbard <jhubbard@nvidia.com>
2026-03-06 16:06:07 -0800

    Add r000 input header for bindings generation
    Version 000 is used as a placeholder for whatever release branch is
    eventually chosen, for our next version of Nova GSP and bootloader
    GPU firmware.
    
    Signed-off-by: John Hubbard <jhubbard@nvidia.com>

---
+++ r000_bindings.h
@@ -0,0 +1,17 @@
+#include "bindings_helper.h"
+#include <cpuopsys.h>
+#include <vgpu/rpc_global_enums.h>
+#include <gpu/gsp/gsp_static_config.h>
+#include <gpu/gsp/gsp_init_args.h>
+#include <generated/g_rpc-message-header.h>
+#include <gpu/gsp/gsp_fw_heap.h>
+#include <gsp/gsp_fw_wpr_meta.h>
+#include <libos_init_args.h>
+#include <generated/g_os_nvoc.h>
+#include <msgq/msgq_priv.h>
+#include <gpu/gsp/message_queue.h>
+#include <gpu/gsp/message_queue_priv.h>
+
+#define RPC_STRUCTURES
+#include <generated/g_rpc-structures.h>
+#include <rmgspseq.h>
